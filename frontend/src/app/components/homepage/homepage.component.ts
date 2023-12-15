a117aa31c0ad7074b0df83a3293811b26e0799ef
Dev <gerrycorbisiero@gmail.com>
2023-12-15 23:51:55 +0100

    refactoring presentation homepage + smooth scroll

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,14 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { SITE_CONFIG } from 'src/app/app.config';
+import { UtilityService } from 'src/app/services/utility/utility.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit, AfterViewInit {
 
   @ViewChild('citazioneContainer', { static: false }) citazioneContainer!: ElementRef;
   @ViewChild('iconCitazioneContainer', { static: false }) iconCitazioneContainer!: ElementRef;
@@ -13,10 +16,23 @@ export class HomepageComponent implements OnInit {
   @ViewChild('lastArticlesSectionParent', { static: false }) lastArticlesSectionParent!: ElementRef;
   @ViewChild('lastArticlesSection', { static: false }) lastArticlesSection!: ElementRef;
 
-  constructor() { }
+  revisoreLegaleFlag: boolean = SITE_CONFIG.revisoreLegale
+
+  constructor(
+    private utilityService: UtilityService,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) { }
 
   ngOnInit(): void {
     this.handleAnimations();
+
+    this.utilityService.scrollToTop();
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+        this.smoothScrollingToAnchor();
+    }
   }
 
   handleAnimations(){
@@ -76,5 +92,33 @@ export class HomepageComponent implements OnInit {
       }
     }, 1000);
   }
+
+      // SMOOTH SCROLLING TO ANCHOR
+      smoothScrollingToAnchor(){
+    
+        document.querySelectorAll('.aSmoothScroll').forEach(anchor => {
+          anchor.addEventListener('click', function (e) {
+              e.preventDefault();
+      
+              let anchor: any = e.target;
+              if(anchor && anchor.hash){
+                let href:any= anchor.hash;
+                // Recupera l'elemento di destinazione
+                const element = document.querySelector(href);
+                if (element) {
+                  // Calcola l'offset in modo personalizzato (50px in meno rispetto al top dell'elemento)
+                  const offset = element.getBoundingClientRect().top + window.scrollY - 150;
+
+                  // Effettua lo scroll con l'offset corretto
+                  window.scrollTo({
+                    top: offset,
+                    behavior: 'smooth',
+                  });
+                }
+              }
+              
+          });
+      });
+      }
 
 }
