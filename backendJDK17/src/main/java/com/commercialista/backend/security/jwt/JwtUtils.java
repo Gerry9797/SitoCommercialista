463d767ff4e9f6bf7bc2f0dcc44887dadce2c1a0
Dev <gerrycorbisiero@gmail.com>
2024-06-25 01:48:08 +0200

    application yaml

--- backendJDK17/src/main/java/com/commercialista/backend/security/jwt/JwtUtils.java
+++ backendJDK17/src/main/java/com/commercialista/backend/security/jwt/JwtUtils.java
@@ -19,10 +19,10 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${configuration.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
+  @Value("${configuration.app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
   public String generateJwtToken(Authentication authentication) {
