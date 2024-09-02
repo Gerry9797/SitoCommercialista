737f63362c18f84191f928a11a3609d5be324cc2
Dev <gerrycorbisiero@gmail.com>
2024-09-03 01:18:12 +0200

    fix animazioni calendario

--- frontend/src/app/components/prenota-consulenza/prenota-consulenza.component.ts
+++ frontend/src/app/components/prenota-consulenza/prenota-consulenza.component.ts
@@ -3,6 +3,7 @@ import { timeout } from 'rxjs';
 import { NgxPopperjsTriggers, NgxPopperjsPlacements } from 'ngx-popperjs';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { UtilityService } from '../../services/utility/utility.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 interface City {
   name: string;
@@ -12,13 +13,32 @@ interface City {
 @Component({
   selector: 'app-prenota-consulenza',
   templateUrl: './prenota-consulenza.component.html',
-  styleUrl: './prenota-consulenza.component.css'
+  styleUrl: './prenota-consulenza.component.css',
+  animations: [
+    trigger('fadeOut', [
+      state('visible', style({
+        opacity: 1,
+        display: 'block'
+      })),
+      state('hidden', style({
+        opacity: 0,
+        display: 'none'
+      })),
+      transition('visible => hidden', [
+        animate('500ms ease-out')
+      ]),
+      transition('hidden => visible', [
+        animate('500ms 600ms ease-in') // 600ms delay before becoming visible
+      ])
+    ])
+  ]
 })
 export class PrenotaConsulenzaComponent implements OnInit {
 
     calendarLoading: boolean = false;
     calendarReady: boolean = false;
     calendarDaySelected: number|undefined = undefined;
+    isLeftMenuCalendarClosed: boolean = false;
 
     formCalendar!: FormGroup;
 
@@ -59,7 +79,11 @@ export class PrenotaConsulenzaComponent implements OnInit {
       //carica qui la configurazione dei giorni/mesi dal backend
       setTimeout(() => {
         this.calendarReady = true;
-      }, 3000);
+      }, 2000);
       
+    }
+
+    toggleLeftMenuCalendar() {
+      this.isLeftMenuCalendarClosed = !this.isLeftMenuCalendarClosed;
     }
 }
